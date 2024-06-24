fc5d73038627164e05ffc06555f3aec5904525a7
MathStrike <mathysbgames@gmail.com>
2024-06-24 11:14:46 +0200

    question 1-3 avec commentaire

--- projet1/python/question_1-3.py
+++ projet1/python/question_1-3.py
@@ -1,54 +1,46 @@
 import pandas as pd
 import os
-import platform
 import math
 
 from get_path import getPath
 
-###
-#Pour que ça marche avec le chemind d'accès
 # Charger le fichier Excel
-chemin_fichier = '/../data/Tableau données sac à dos. Vélo.xlsx'  # Chemin ajusté
+chemin_fichier = '/../data/Tableau données sac à dos. Vélo.xlsx'  
 
+#Pour ajuster le chemin d'accès
 script_dir = os.path.dirname(os.path.abspath(__file__))
 script_dir, file_dir = getPath(script_dir, chemin_fichier)
-
-
-
 data = pd.read_excel(os.path.join(script_dir + chemin_fichier))
-
-
-
 #//////////////////////////////////////////////////////////////////////
 
 
 print("Question 1")
 
-# Définir la fonction pour calculer le nombre de combinaisons
+# Définir la fonction pour déterminer combien on peut faire de sacs de n objets
+# avec 23 objets
 def nombre_de_combinaisons(N):
     return math.factorial(23)/(math.factorial(N)*math.factorial(23-N))
 
-
-# Définir la liste des valeurs de N
+# Définir la liste des valeurs du nombre d'objets par sac
 valeurs_N = [1, 2, 10, 23]
 
-# Calculer et imprimer le nombre de combinaisons pour chaque valeur de N
 resultats = {N: nombre_de_combinaisons(N) for N in valeurs_N}
 
-
+#Affiche le nombre de sac de N objets qu'on peut faire avec 23 objets
 for N, combinaisons in resultats.items():
     print(f"Le nombre de sac de {N} objet est {combinaisons} sacs")
 
 #//////////////////////////////////////////////////////////////////////
 
 print("Question 2")
+
+#on fait la somme du nombre de sacs de 0 à 23 objets qu'on faire avec 23 objets
 def somme_combinaisons():
     somme = 0
     for N in range(24):  # De 0 à 23 inclus
         somme += nombre_de_combinaisons(N)
     return somme
-
-# Exemple d'utilisation
+#affichage du résultat
 print(f"La somme des combinaisons de 0 à 23 est {somme_combinaisons()}")    
 
 
