dcbd7bf6c5dd7ee721c9fd0a1907b9d10604a803
MathStrike <mathysbgames@gmail.com>
2024-06-24 11:45:51 +0200

    question 4 à 6

--- projet1/python/question_6.py
+++ projet1/python/question_6.py
@@ -1,5 +1,8 @@
 import time
 
+
+
+
 def somme_premiers_nombres(n):
     somme = 0
     nombre_operations = 0
@@ -16,14 +19,14 @@ fin = time.time()
 duree = fin - debut
 temps_par_operation = duree / operations
 
-# Nombre total d'objets
-N = 23
 
 # Calculer le nombre total d'organisations possibles
-nombre_organisations = 2 ** N
+nombre_organisations = 8388608.0
 
 # Calculer le temps total nécessaire
 temps_total = nombre_organisations * temps_par_operation
+
+#:.12f formatte le nombre temps_par_operation pour qu'il soit affiché avec exactement 12 chiffres après la virgule décimale.
 
 print(f"Nombre total d'organisations possibles: {nombre_organisations}")
 print(f"Temps moyen par opération: {temps_par_operation:.12f} secondes")
