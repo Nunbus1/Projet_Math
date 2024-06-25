a5c122a6200a3472ad72a9adfac2e25bc454e700
GUERMEUR-Nicolas <91013253+GUERMEUR-Nicolas@users.noreply.github.com>
2024-06-25 10:15:54 +0200

    Update bin_item.py

--- projet2/python/bin_item.py
+++ projet2/python/bin_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from decimal import *
+from functools import reduce
 import numpy as np
 #import fcl
 
@@ -26,8 +27,10 @@ class Bin:
 	def __init__(self, dimensions, items=list(), bb=list()):
 		self.dimensions = dimensions
 		self.items = items
-		self.items_bb = bb
 		self.is_open = True
+		self.dims = [self.x]
+		if dimensions >= 2: self.dims.append(self.y)
+		if dimensions >= 3: self.dims.append(self.z)
 
 	def __repr__(self):
 		return f"<Bin dimensions:{self.dimensions} is_open:{self.is_open}>"
@@ -39,18 +42,26 @@ class Bin:
 		return self.items
 
 	def can_fit(self, new_item):
-		return sum([sum(item.dims) for item in self.items]) + sum(new_item.dims) < product
-#		if self.dimensions == 1:
-#			return sum([item.x for item in self.items]) + new_item.x < self.x
-#		elif self.dimensions == 2:
-#			return sum([item.x * item.y for item in self.items]) + new_item.x*new_item.y < self.x*self.y
-#		elif self.dimensions == 3:
-#			return sum([item.x * item.y * item.z for item in self.items]) + new_item.x*new_item.y*new_item.z < self.x*self.y*self.z
-#		else:
-#			return "ERROR"
+		#t= (sum([reduce(lambda x, y: x*y, item.dims) for item in self.items]) + reduce(lambda x, y: x*y, new_item.dims), reduce(lambda x, y: x*y, self.dims))
+		if self.dimensions == 1:
+			t2= (sum([item.x for item in self.items]) + new_item.x, self.x)
+#			if t != t2:
+#				print(self.dims)
+#				print(self.items)
+#				print(t, t2)
+			return t2[0] < t2[1]
+		elif self.dimensions == 2:
+			t2= (sum([item.x * item.y for item in self.items]) + new_item.x*new_item.y, self.x*self.y)
+#			if t != t2:
+#				print(t, t2)
+			return t2[0] < t2[1]
+		elif self.dimensions == 3:
+			t2= (sum([item.x * item.y * item.z for item in self.items]) + new_item.x*new_item.y*new_item.z, self.x*self.y*self.z)
+#			if t != t2:
+#				print(t, t2)
+			return t2[0] < t2[1]
+		else:
+			return "ERROR"
 
 	def is_empty(self):
 		return len(self.items) == 0
-
-	def remaining_space(self):
-		return
